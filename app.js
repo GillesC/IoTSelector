f1f661873c9dd30f0103b182b337428035dbf332
GillesCallebaut <callebaut.gilles@gmail.com>
2017-04-06 09:53:54 +0200

    added comments to readContent

--- app.js
+++ app.js
@@ -45,6 +45,8 @@ app.use(function (err, req, res, next) {
 
 
 /**********************************************************/
+var technologyArray = [];
+var serviceDefArray = [];
 
 function readContents() {
     const jsonfile = require('jsonfile');
@@ -56,21 +58,61 @@ function readContents() {
     const serviceDefPath = rootFolder + '/' + serviceDefDir;
 
     const servicesDir = "services";
-    const servicesPath = rootFolder + '/' +serviceDefDir;
+    const servicesPath = rootFolder + '/' + serviceDefDir;
 
     const technologiesDir = "technologies";
-    const technologiesPath = rootFolder + '/'+technologiesDir;
+    const technologiesPath = rootFolder + '/' + technologiesDir;
 
-    fs.readdir(technologiesPath, function (err, files) {
-        files.forEach(function (file) {
-            console.log(file);
-            var content = jsonfile.readFileSync(technologiesPath+"\\"+file);
-            console.log(content);
-        });
+    /**
+     * Read all technologies from technologiesPath
+     * and put them in the technologyArray with as index
+     * the technology ID
+     *
+     */
+    fs.readdirSync(technologiesPath).forEach(function (technologyFileName) {
+        console.log(technologyFileName);
+        var technology = jsonfile.readFileSync(technologiesPath + "\\" + technologyFileName);
+        technologyArray[technology.ID] = technology;
+        console.log(technology);
+    });
+
+
+    /**
+     * Read all service definitions (e.g. range, roaming, security)
+     * and put them in the serviceDefArray with as index
+     * the service definition ID
+     *
+     */
+    fs.readdirSync(serviceDefPath).forEach(function (serviceDefFileName) {
+        console.log(serviceDefFileName);
+        var serviceDef = jsonfile.readFileSync(serviceDefPath + "\\" + serviceDefFileName);
+        serviceDefArray[serviceDef.ID] = serviceDef;
+        console.log(serviceDef);
+    });
+
+
+    /**
+     * Read all services (delivered by technologies)
+     * and add them to the corresponding technology
+     * (in their member var services)
+     *
+     */
+    fs.readdirSync(servicesPath).forEach(function (serviceFileName) {
+        console.log(serviceFileName);
+        var service = jsonfile.readFileSync(serviceDefPath + "\\" + serviceFileName);
+        var technology = technologyArray[service.technologyID];
+        technology.services.push(service);
+        console.log(service);
     });
 
 
 }
+
+
 readContents();
+console.log("-----------------------------");
+console.log(technologyArray[1]);
+console.log(serviceDefArray[1]);
+
 /**********************************************************/
 module.exports = app;
